2f66a2ab3e7175c3d74f6f13a1922c825270d2ea
Andrea Brandao Duque <abd@debian.deabian>
2017-01-12 17:31:37 -0300

    List of freq motifs in a Corpus

--- motif.R
+++ motif.R
@@ -5,14 +5,16 @@ dir <- "/home/abd/Documentos/gutenberg-mining"
 setwd(dir)
 source("R/graphs-utils.R")
 
-myCorpus <- Corpus(VectorSource("three men wait door say holmes oh indeed 
-                                seem do thing completely must compliment holmes answer"))
+#Easy visualization example
+#myCorpus <- Corpus(VectorSource("three men wait door say holmes oh indeed 
+                                #seem do thing completely must compliment holmes answer"))
 
 #creates book network graph list
 graphs <- corpus_to_graphs(myCorpus)
 nodes_labels <- get_nodes_labels(myCorpus)
 #visualize graph
-plot(graphs[[1]], vertex.label=nodes_labels[[1]])
+plot(graphs[[1]], vertex.label=NA, vertex.size=0.5, edge.arrow.size=0.2, edge.width=0.2, edge.arrow.width=0.2)
 
+list_freq_motifs <- lapply(graphs, motifs, size = 3)
 #frequencies of ternary motifs
 freq_motifs <- motifs(graphs[[1]], size = 3)
