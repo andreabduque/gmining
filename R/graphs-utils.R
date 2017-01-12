0746f39970fca7e441bae30b24267f460fb9fe18
Andrea Brandao Duque <abd@debian.deabian>
2017-01-12 12:04:37 -0300

    some graph functions created

--- R/graphs-utils.R
+++ R/graphs-utils.R
@@ -1,22 +1,58 @@
 library(igraph)
 library(rowr)
+library(tm)
+library(dplyr)
+library(stringr)
+library(NLP)
+library(tokenizers)
 
-#given two pair of words creates an edge
+#given a pair of words creates an edge
 create_edge <- function(pair, node_labels) {
   e <- Map(c, match(pair[1], node_labels), match(pair[2], node_labels))
   return(e)
 }
 
-#creates a graph for a book given the nodes
-create_graph <- function(x) {
-  
-  #text_vector <- unlist(strsplit(as.character(x), "\\s+"))
-  book_content <- tokenize_words(as.character(x$content))[[1]]
+#create nodes from a book content
+create_nodes <- function(book_content){
+  book_tokens <- tokenize_words(as.character(book_content))[[1]]
   #computing nodes
-  nodes <- unique(book_content)
+  nodes <- unique(book_tokens)
+  
+  return (nodes)
+}
+
+#creates a edge list for a book given the nodes
+create_edge_list <- function(book_content) {
+  #computing unique words in book
+  book_tokens <- tokenize_words(as.character(book_content))[[1]]
+  #computing graph nodes
+  nodes <- create_nodes(book_content)
   #computing edges
-  edge_list <- rollApply(book_content, create_edge, node_labels=nodes, window=2)
+  edge_list <- rollApply(book_tokens, create_edge, node_labels=nodes, window=2)
+  #removing non existing edge
+  edge_list <- head(edge_list, -1)
   
   return (edge_list)
 }
-
+#creates graph from a edge list
+create_graph <- function(edge_list) {
+  #convert list to matrix
+  mat_edge_list <- do.call(rbind, edge_list)
+  #generate graph from connectivity matrix
+  graph <- graph_from_edgelist(mat_edge_list, directed = TRUE)
+  
+  return (graph)
+}
+#returns a list of graphs given a book corpus
+corpus_to_graphs <- function(myCorpus) {
+  list_network <- lapply(myCorpus$content, create_edge_list)
+  graphs <-  lapply(list_network, create_graph)
+  
+  return (graphs)
+}
+#get nodes labels for each book graph in a corpus
+get_nodes_labels <- function(myCorpus) {
+  nodes_labels <- lapply(myCorpus$content, create_nodes)
+  
+  return(nodes_labels)
+}
--- motif.R
+++ motif.R
@@ -1,34 +1,18 @@
-#calculating motif frequencies in book Corpus
+#calculating motif frequencies in a book Corpus
 
 #Setting working Directory
 dir <- "/home/abd/Documentos/gutenberg-mining"
 setwd(dir)
 source("R/graphs-utils.R")
 
-
-
 myCorpus <- Corpus(VectorSource("three men wait door say holmes oh indeed 
                                 seem do thing completely must compliment holmes answer"))
 
-#unique(tokenize_words(song)[[1]])
-
-
-
-#extract terms from text
-#tdm <- TermDocumentMatrix(myCorpus, control=list(wordLengths=c(1,Inf)))
-
-#creates edge list
-list_network <- lapply(myCorpus, create_graph)
-list_network <- head(list_network[[1]], -1)
-
-#convert list to matrix
-mat_list_network <- do.call(rbind, list_network)
-
-#generate graph from connectivity matrix
-graph <- graph_from_edgelist(mat_list_network, directed = TRUE)
-
+#creates book network graph list
+graphs <- corpus_to_graphs(myCorpus)
+nodes_labels <- get_nodes_labels(myCorpus)
 #visualize graph
-plot(graph, vertex.label=nodes)
+plot(graphs[[1]], vertex.label=nodes_labels[[1]])
 
 #frequencies of ternary motifs
-freq_motifs <- motifs(graph, size = 3)
+freq_motifs <- motifs(graphs[[1]], size = 3)
