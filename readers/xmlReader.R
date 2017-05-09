6945e666912941d8dad6ac8d567584559f90c460
andreabduque <abd@cin.ufpe.br>
2017-05-09 09:49:15 -0300

    Changing corpus

---
+++ readers/xmlReader.R
@@ -0,0 +1,29 @@
+library(readxl)
+library(XML)
+library(parallel)
+path_list <- c("all/small")
+dir <- DirSource(path_list)
+
+#https://rpubs.com/msundar/large_data_analysis
+
+setwd("/home/abd/corpora/corpus-SHC/all")
+
+shc_metadata <- read_excel("/home/abd/corpora/corpus-SHC/SHC_Playlist.xlsx")
+
+all_files <- lapply(shc_metadata$filenumber, function(x) {
+              algo <- paste0(x, ".xml")
+              return(algo)
+              } )
+
+all_tokens <- mclapply(all_files, function(x){
+                if(file.exists(x)){
+                  doc <- xmlInternalTreeParse(x)
+                  tokens <- xpathApply(doc, "//*[@lemma]", xmlGetAttr, "lemma")
+                }
+                else{
+                  tokens <- NULL
+                }
+                
+                return(tokens)
+              }, mc.cores=3)
+
